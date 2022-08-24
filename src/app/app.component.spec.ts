ce8219b5d83f3c6656654aa55b014f6cee11108a
samihabbaba <sami.software@hotmail.com>
2022-08-24 16:16:27 +0300

    adpl name changes and firebase config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mave-ng'`, () => {
+  it(`should have as title 'adpl_meta'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mave-ng');
+    expect(app.title).toEqual('adpl_meta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mave-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('adpl_meta app is running!');
   });
 });
